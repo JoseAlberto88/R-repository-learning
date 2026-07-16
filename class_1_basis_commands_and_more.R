8fb5f84f83bd602c2690d132e263220db839eebc
Jose <jose_alberto_mm@outlook.com>
2026-07-16 13:23:51 -0400

    feat: Some new concepts

---
+++ class_1_basis_commands_and_more.R
@@ -0,0 +1,21 @@
+# --- Basic concepts to know from the start ----
+
+print("Hello")
+
+
+# --- RStudio is case sensitive ----
+# Assignment: Use <- to assign values (assignment operator)
+x <- 10
+y = 3
+a <- c(10,2)
+c <- 3
+d <- 4
+
+# you can do this as well in the console
+
+# Remove the vector or data frame from Global environment
+rm(x)
+rm(a, c, d)  # we can do this in the console
+
+# Click on the broom icon in your global environment to delete all the variables
+
